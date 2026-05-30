1cbd80bd069d2d33312a8ead86c90cbe470503bf
Jeany <il.karachev@gmail.com>
2026-05-30 13:38:16 +0300

    Add remaining unit emojis

--- models/emoji.py
+++ models/emoji.py
@@ -5,3 +5,62 @@ class CustomEmoji:
 
     def __str__(self):
         return f"<tg-emoji emoji-id='{self.custom_emoji_id}'>{self.base_emoji}</tg-emoji>"
+
+class UnitsEmoji:
+    carrier = CustomEmoji(
+        "5249050334701068122",
+        "🚀"
+    )
+
+    dreadnought = CustomEmoji(
+        "5249449285623258726",
+        "🚀"
+    )
+
+    cruiser = CustomEmoji(
+        "5246936437697321098",
+        "🚀"
+    )
+
+    fighter = CustomEmoji(
+        "5249158250049345965",
+        "🚀"
+    )
+
+    destroyer = CustomEmoji(
+        "5249191359952231178",
+        "🚀"
+    )
+
+    warsun = CustomEmoji(
+        "5229040764968805275",
+        "🌞"
+    )
+
+    flagship = CustomEmoji(
+        "5229066702276304464",
+        "🚀"
+    )
+
+    infantry = CustomEmoji(
+        "5249443650626164757",
+        "🤖"
+    )
+
+    mech = CustomEmoji(
+        "5230997083917423709",
+        "🤖"
+    )
+
+    pds = CustomEmoji(
+        "5247012205215389641",
+        "🏠"
+    )
+
+    space_dock = CustomEmoji(
+        "5246912922751377208",
+        "🏠"
+    )
+
+
+
