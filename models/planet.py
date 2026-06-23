9aac1f6ddf3dbda9fd629157e99cba0efef6204b
Jeany <il.karachev@gmail.com>
2026-06-23 16:52:15 +0300

    feat: populate planet

--- models/planet.py
+++ models/planet.py
@@ -1,12 +1,34 @@
+from dataclasses import dataclass
+from enum import Enum
+
+
+class Techskip(str, Enum):
+    RED = "red"
+    GREEN = "green"
+    BLUE = "blue"
+    YELLOW = "yellow"
+
+@dataclass
 class Planet:
     name: str
     resource: int
     influence: int
+    is_home_system: bool = False
+    is_legendary: bool = False
+    techskip: Techskip | None = None
 
-    def __init__(self, name: str, resource: int, influence: int):
-        self.name = name
-        self.resource = resource
-        self.influence = influence
 
     def __str__(self):
         return f"{self.name} ({self.resource}/{self.influence})"
+
+
+    @property
+    def optimal(self):
+        if self.resource > self.influence:
+            return self.resource, 0
+
+        elif self.resource < self.influence:
+            return 0, self.influence
+
+        else:
+            return self.resource, self.influence
