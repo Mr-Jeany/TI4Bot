f85eb0d6d021bf8d2965c05c98208ed3761417d6
Jeany <il.karachev@gmail.com>
2026-06-23 20:03:46 +0300

    feat: add tile and slice classes
    [skip ci]

---
+++ models/slice.py
@@ -0,0 +1,38 @@
+from PIL import Image, ImageDraw
+
+from models.tile import Tile
+
+
+class Slice:
+    """
+    A class for Milty draft slices
+    """
+
+    # Indexes:
+    # 1-3 - first row above home system, home system is 0
+    # 4-5 - start of the next row
+    tiles: list[Tile | None]
+
+
+    def generate_image(self, images) -> Image.Image:
+        width, height = images[0].size
+        center_width = int(width / 4 * 3)
+
+        coordinates = {
+            0: (center_width, 2*height),
+            1: (0, int(height * 1.5)),
+            2: (center_width, height),
+            3: (int(width * 1.5), int(height * 1.5)),
+            4: (0, int(height * 0.5)),
+            5: (center_width, 0),
+        }
+
+        base = Image.new(
+            "RGBA",
+            (int(width * 2.5), height * 3)
+        )
+
+        for index in range(len(images)):
+            base.alpha_composite(images[index], coordinates[index])
+
+        return base
---
+++ models/tile.py
@@ -0,0 +1,40 @@
+from dataclasses import dataclass
+from enum import Enum
+
+from PIL import Image
+
+from models.planet import Planet
+
+class TileType(str, Enum):
+    RED = "red" # no planets
+    BLUE = "blue" # with planet(s)
+
+@dataclass
+class Tile:
+    id: int
+    planets: list[Planet]
+    is_home_system: int
+    tile_type: TileType
+    is_anomaly: bool = False
+    image: Image.Image | None = None
+
+    contains_legendary: bool = False
+
+    def __post_init__(self):
+        self.contains_legendary = any(
+            planet.is_legendary for planet in self.planets
+        )
+
+
+    @property
+    def optimal(self) -> tuple[int, int]:
+        if not self.planets:
+            return 0, 0
+
+        resource = 0
+        influence = 0
+        for planet in self.planets:
+            resource += planet.resource
+            influence += planet.influence
+
+        return resource, influence
